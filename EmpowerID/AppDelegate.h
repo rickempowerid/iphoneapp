f2e4b40e20049cc31c55254066be27a716ac71c0
R <rick@Rs-Mac.local>
2013-05-01 19:09:28 -0700

    Initial Commit

---
+++ EmpowerID/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  EmpowerID
+//
+//  Created by R on 5/1/13.
+//  Copyright (c) 2013 R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
